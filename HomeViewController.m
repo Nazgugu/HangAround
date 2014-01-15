18625f406390642c18cbd339c58713bd073da058
Nazgugu <prada520farrell@gmail.com>
2014-01-15 13:43:01 -0800

    New Image corper, few things need to be fixed, next scheduled push in minutes

--- HomeViewController.m
+++ HomeViewController.m
@@ -12,8 +12,13 @@
 #import "TGRImageZoomAnimationController.h"
 #import "UIImage+Resize.h"
 #import "Singleton.h"
+#import "VPImageCropperViewController.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface HomeViewController ()<UIViewControllerTransitioningDelegate>
+#define ORIGINAL_MAX_WIDTH 640.0f
+
+@interface HomeViewController ()<UIViewControllerTransitioningDelegate, VPImageCropperDelegate, UIAlertViewDelegate>
 {
     BOOL isFullScreen;
     dispatch_queue_t queue;
@@ -90,35 +95,119 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark VPImageCropperDelegate
+- (void)imageCropper:(VPImageCropperViewController *)cropperViewController didFinished:(UIImage *)editedImage {
+    [self resizeImage:editedImage];
+    //isFullScreen = NO;
+    [UserImage setImage:[Singleton globalData].avatar];
+    [cropperViewController dismissViewControllerAnimated:YES completion:^{
+        queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+        dispatch_async(queue, ^{
+            [self uploadImage];
+            isFullScreen = NO;
+        });
+    }];
+}
+
+- (void)imageCropperDidCancel:(VPImageCropperViewController *)cropperViewController {
+    [cropperViewController dismissViewControllerAnimated:YES completion:^{
+    }];
+}
+
+
 #pragma mark - image picker delegte
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
 {
 	
+    [picker dismissViewControllerAnimated:YES completion:^()
+    {
+        UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
+        image = [self imageByScalingToMaxSize:image];
+        VPImageCropperViewController *ImgEditorVC = [[VPImageCropperViewController alloc] initWithImage:image cropFrame:CGRectMake(0, 100.0f, self.view.frame.size.width, self.view.frame.size.height - 250.0f) limitScaleRatio:2.5];
+        ImgEditorVC.delegate = self;
+        [self presentViewController:ImgEditorVC animated:YES completion:^{
+            ImgEditorVC.hidesBottomBarWhenPushed = YES;
+            isFullScreen = YES;
+        }];
+    }];
     
-    UIImage *image = [info objectForKey:UIImagePickerControllerOriginalImage];
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        CGFloat height = 480.0f;
-        CGFloat width = (height / image.size.height) * image.size.width;
-        UIImage *newImage = [image resizedImage:CGSizeMake(width, height) interpolationQuality:kCGInterpolationDefault];
-        [Singleton globalData].avatar = newImage;
-    
-        dispatch_async(dispatch_get_main_queue(), ^{
-            //NSString *fullPath = [[NSHomeDirectory() stringByAppendingPathComponent:@"Documents"] stringByAppendingPathComponent:@"currentImage.png"];
-    
-            //UIImage *savedImage = [[UIImage alloc] initWithContentsOfFile:fullPath];
-    
-            isFullScreen = NO;
-            [UserImage setImage:newImage];
-        });
-    
-    });
-    queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
-    dispatch_async(queue, ^{
-        [self uploadImage];
-    });
-    [picker dismissViewControllerAnimated:YES completion:nil];
 }
+
+- (void)resizeImage:(UIImage *)image
+{
+    CGFloat height = 480.0f;
+    CGFloat width = (height / image.size.height) * image.size.width;
+    UIImage *newImage = [image resizedImage:CGSizeMake(width, height) interpolationQuality:kCGInterpolationDefault];
+    [Singleton globalData].avatar = newImage;
+}
+
+#pragma mark image scale utility
+- (UIImage *)imageByScalingToMaxSize:(UIImage *)sourceImage {
+    if (sourceImage.size.width < ORIGINAL_MAX_WIDTH) return sourceImage;
+    CGFloat btWidth = 0.0f;
+    CGFloat btHeight = 0.0f;
+    if (sourceImage.size.width > sourceImage.size.height) {
+        btHeight = ORIGINAL_MAX_WIDTH;
+        btWidth = sourceImage.size.width * (ORIGINAL_MAX_WIDTH / sourceImage.size.height);
+    } else {
+        btWidth = ORIGINAL_MAX_WIDTH;
+        btHeight = sourceImage.size.height * (ORIGINAL_MAX_WIDTH / sourceImage.size.width);
+    }
+    CGSize targetSize = CGSizeMake(btWidth, btHeight);
+    return [self imageByScalingAndCroppingForSourceImage:sourceImage targetSize:targetSize];
+}
+
+- (UIImage *)imageByScalingAndCroppingForSourceImage:(UIImage *)sourceImage targetSize:(CGSize)targetSize {
+    UIImage *newImage = nil;
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = targetSize.width;
+    CGFloat targetHeight = targetSize.height;
+    CGFloat scaleFactor = 0.0;
+    CGFloat scaledWidth = targetWidth;
+    CGFloat scaledHeight = targetHeight;
+    CGPoint thumbnailPoint = CGPointMake(0.0,0.0);
+    if (CGSizeEqualToSize(imageSize, targetSize) == NO)
+    {
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        if (widthFactor > heightFactor)
+            scaleFactor = widthFactor; // scale to fit height
+        else
+            scaleFactor = heightFactor; // scale to fit width
+        scaledWidth  = width * scaleFactor;
+        scaledHeight = height * scaleFactor;
+        
+        // center the image
+        if (widthFactor > heightFactor)
+        {
+            thumbnailPoint.y = (targetHeight - scaledHeight) * 0.5;
+        }
+        else
+            if (widthFactor < heightFactor)
+            {
+                thumbnailPoint.x = (targetWidth - scaledWidth) * 0.5;
+            }
+    }
+    UIGraphicsBeginImageContext(targetSize); // this will crop
+    CGRect thumbnailRect = CGRectZero;
+    thumbnailRect.origin = thumbnailPoint;
+    thumbnailRect.size.width  = scaledWidth;
+    thumbnailRect.size.height = scaledHeight;
+    
+    [sourceImage drawInRect:thumbnailRect];
+    
+    newImage = UIGraphicsGetImageFromCurrentImageContext();
+    if(newImage == nil) NSLog(@"could not scale image");
+    
+    //pop the context to get back to the default
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
 
 - (void)uploadImage
 {
@@ -145,6 +234,8 @@
                            if (!error){
                                object[@"imageFile"] = imageFile;
                                [object saveInBackground];
+                               UIAlertView *imageUpdate = [[UIAlertView alloc] initWithTitle:@"Grats" message:@"Successfully updated your avatar" delegate:self cancelButtonTitle:@"Done" otherButtonTitles: nil];
+                               [imageUpdate show];
                                NSLog(@"Can you see me?");
                            }
                        }];
@@ -159,6 +250,8 @@
                           if (!error)
                           {
                               //upload succeed
+                              UIAlertView *firstTimeUpLoad = [[UIAlertView alloc] initWithTitle:@"Grats" message:@"Successfully uploaded first photo" delegate:self cancelButtonTitle:@"Done" otherButtonTitles: nil];
+                              [firstTimeUpLoad show];
                           }
                            else
                            {
@@ -287,25 +380,91 @@
     }
     if (actionSheet.tag == 2)
     {
-        // check if support camara
-        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-            
-            switch (buttonIndex) {
-                case 2:
-                    // cancel
-                    return;
-                case 0:
-                    // camara
-                    [self choosePhotoFromCamera];
-                    break;
-                    
-                case 1:
-                    // library
-                    [self choosePhotoFromLibrary];
-                    break;
-            }
+            //0 is camera, 1 is library,2 is cancel
+        if (buttonIndex == 0)
+        {
+            if ([self isCameraAvailable] && [self doesCameraSupportTakingPhotos]) {
+                UIImagePickerController *controller = [[UIImagePickerController alloc] init];
+                controller.sourceType = UIImagePickerControllerSourceTypeCamera;
+                if ([self isFrontCameraAvailable]) {
+                    controller.cameraDevice = UIImagePickerControllerCameraDeviceFront;
+                }
+                NSMutableArray *mediaTypes = [[NSMutableArray alloc] init];
+                [mediaTypes addObject:(__bridge NSString *)kUTTypeImage];
+                controller.mediaTypes = mediaTypes;
+                controller.delegate = self;
+                [self presentViewController:controller
+                                   animated:YES
+                                 completion:^(void){
+                                     NSLog(@"Picker View Controller is presented");
+                                 }];
+
+        }
+        }
+        else if (buttonIndex == 1)
+        {
+            if ([self isPhotoLibraryAvailable]) {
+                UIImagePickerController *controller = [[UIImagePickerController alloc] init];
+                controller.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+                NSMutableArray *mediaTypes = [[NSMutableArray alloc] init];
+                [mediaTypes addObject:(__bridge NSString *)kUTTypeImage];
+                controller.mediaTypes = mediaTypes;
+                controller.delegate = self;
+                [self presentViewController:controller
+                                   animated:YES
+                                 completion:^(void){
+                                     NSLog(@"Picker View Controller is presented");
+                                 }];
+
+        }
         }
     }
+}
+        
+#pragma mark camera utility
+- (BOOL) isCameraAvailable{
+    return [UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera];
+}
+
+- (BOOL) isRearCameraAvailable{
+    return [UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceRear];
+}
+
+- (BOOL) isFrontCameraAvailable {
+    return [UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceFront];
+}
+
+- (BOOL) doesCameraSupportTakingPhotos {
+    return [self cameraSupportsMedia:(__bridge NSString *)kUTTypeImage sourceType:UIImagePickerControllerSourceTypeCamera];
+}
+
+- (BOOL) isPhotoLibraryAvailable{
+    return [UIImagePickerController isSourceTypeAvailable:
+            UIImagePickerControllerSourceTypePhotoLibrary];
+}
+- (BOOL) canUserPickVideosFromPhotoLibrary{
+    return [self
+            cameraSupportsMedia:(__bridge NSString *)kUTTypeMovie sourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+}
+- (BOOL) canUserPickPhotosFromPhotoLibrary{
+    return [self
+            cameraSupportsMedia:(__bridge NSString *)kUTTypeImage sourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+}
+
+- (BOOL) cameraSupportsMedia:(NSString *)paramMediaType sourceType:(UIImagePickerControllerSourceType)paramSourceType{
+    __block BOOL result = NO;
+    if ([paramMediaType length] == 0) {
+        return NO;
+    }
+    NSArray *availableMediaTypes = [UIImagePickerController availableMediaTypesForSourceType:paramSourceType];
+    [availableMediaTypes enumerateObjectsUsingBlock: ^(id obj, NSUInteger idx, BOOL *stop) {
+        NSString *mediaType = (NSString *)obj;
+        if ([mediaType isEqualToString:paramMediaType]){
+            result = YES;
+            *stop= YES;
+        }
+    }];
+    return result;
 }
 
 
